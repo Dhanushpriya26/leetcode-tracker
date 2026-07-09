6549dd4af3ddc5e541fc403d26ed105ac6d71749
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-09 09:17:35 +0530

    Create 404-SumOfLeftLeaves

---
+++ 404-SumOfLeftLeaves/404-SumOfLeftLeaves.java
@@ -0,0 +1,31 @@
+// Last updated: 7/9/2026, 9:17:36 AM
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        Queue<Pair<TreeNode, Boolean>> queue = new LinkedList<>();
+        queue.offer(new Pair<>(root, false));  // (node, is_left)
+        int totalSum = 0;
+        
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, Boolean> pair = queue.poll();
+            TreeNode node = pair.getKey();
+            boolean isLeft = pair.getValue();
+            
+            if (isLeft && node.left == null && node.right == null) {
+                totalSum += node.val;
+            }
+            
+            if (node.left != null) {
+                queue.offer(new Pair<>(node.left, true));
+            }
+            if (node.right != null) {
+                queue.offer(new Pair<>(node.right, false));
+            }
+        }
+        
+        return totalSum;
+    }
+}
