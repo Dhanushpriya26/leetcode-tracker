408e1b76b582fed44bdab7fc60a4315192acea03
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-09 09:16:37 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:16:37 AM
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int duplicate = -1;
+        int missing = -1;        
+        for (int i = 1; i <= nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == i) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                duplicate = i;
+            } else if (count == 0) {
+                missing = i;
+            }
+        }
+        return new int[] {duplicate, missing};
+    }
+}
