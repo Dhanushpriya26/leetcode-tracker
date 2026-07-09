f7ba013a1997ba8c7a701c637794d6f81acba463
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-09 09:17:29 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:17:30 AM
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i=num1.length()-1;
+        int j=num2.length()-1;
+        int carry=0;
+        StringBuilder ans=new StringBuilder();
+        while(i>=0||j>=0||carry!=0){
+            int sum=carry;
+            if(i>=0){
+                sum+=num1.charAt(i--)-'0';
+            }
+            if(j>=0){
+                sum+=num2.charAt(j--)-'0';
+            }
+            ans.append(sum%10);
+            carry= sum/10;
+        }
+        return ans.reverse().toString();
+    }
+}
