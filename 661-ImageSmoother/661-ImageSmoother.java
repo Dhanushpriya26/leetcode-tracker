b5e765ac1b353dbd27d4d70aed4c7c147d5afa44
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-09 09:16:35 +0530

    Create 661-ImageSmoother

---
+++ 661-ImageSmoother/661-ImageSmoother.java
@@ -0,0 +1,31 @@
+// Last updated: 7/9/2026, 9:16:35 AM
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+
+        int m = img.length;
+        int n = img[0].length;
+        int[][] ans = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int sum = 0;
+                int count = 0;
+
+                for (int r = i - 1; r <= i + 1; r++) {
+                    for (int c = j - 1; c <= j + 1; c++) {
+
+                        if (r >= 0 && r < m && c >= 0 && c < n) {
+                            sum += img[r][c];
+                            count++;
+                        }
+                    }
+                }
+
+                ans[i][j] = sum / count;
+            }
+        }
+
+        return ans;
+    }
+}
