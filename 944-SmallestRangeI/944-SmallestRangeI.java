6bb4f3355c55ce4f1660a73d1ddf61104abd7023
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-16 09:14:38 +0530

    Create 944-SmallestRangeI

---
+++ 944-SmallestRangeI/944-SmallestRangeI.java
@@ -0,0 +1,11 @@
+// Last updated: 7/16/2026, 9:14:37 AM
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+      int min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
+        for(int val:nums){
+            if(val<min)min=val;
+            if(val>max)max=val;
+        }
+        return (min+k)>(max-k)?0:(max-k)-(min+k);
+    }
+}
