bec44974800deae2f2012c1b6428ba92e9c39749
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-16 09:14:34 +0530

    Create 978-ValidMountainArray

---
+++ 978-ValidMountainArray/978-ValidMountainArray.java
@@ -0,0 +1,9 @@
+// Last updated: 7/16/2026, 9:14:34 AM
+class Solution {
+       public boolean validMountainArray(int[] A) {
+        int n = A.length, i = 0, j = n - 1;
+        while (i + 1 < n && A[i] < A[i + 1]) i++;
+        while (j > 0 && A[j - 1] > A[j]) j--;
+        return i > 0 && i == j && j < n - 1;
+    }
+}
