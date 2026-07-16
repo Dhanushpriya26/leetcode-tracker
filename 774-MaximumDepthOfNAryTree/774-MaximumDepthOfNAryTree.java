fe17462a672364b63cef3f5349fad6dab5b85d35
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-16 09:14:58 +0530

    Create 774-MaximumDepthOfNAryTree

---
+++ 774-MaximumDepthOfNAryTree/774-MaximumDepthOfNAryTree.java
@@ -0,0 +1,11 @@
+// Last updated: 7/16/2026, 9:14:58 AM
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null)
+            return 0;
+        int maxx = 1;
+        for (Node child : root.children)
+            maxx = Math.max(maxx, 1 + maxDepth(child));
+        return maxx;
+    }
+}
