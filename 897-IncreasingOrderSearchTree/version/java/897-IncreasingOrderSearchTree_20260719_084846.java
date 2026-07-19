b2dc4cc6c635636a73e8f34acf213d84be33f715
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-19 08:48:47 +0530

    Create 897-IncreasingOrderSearchTree

---
+++ 897-IncreasingOrderSearchTree/version/java/897-IncreasingOrderSearchTree_20260719_084846.java
@@ -0,0 +1,30 @@
+// Last updated: 7/19/2026, 8:48:46 AM
+1class Solution {
+2    TreeNode newNode = null;
+3    TreeNode prev = null;
+4
+5    public TreeNode increasingBST(TreeNode root) {
+6        inorder(root);
+7        return newNode;
+8    }
+9
+10    public void inorder(TreeNode root) {
+11        if (root == null) return;
+12
+13        inorder(root.left);
+14
+15        if (newNode == null) {
+16            newNode = root;
+17        }
+18
+19        root.left = null;
+20
+21        if (prev != null) {
+22            prev.right = root;
+23        }
+24
+25        prev = root;
+26
+27        inorder(root.right);
+28    }
+29}
