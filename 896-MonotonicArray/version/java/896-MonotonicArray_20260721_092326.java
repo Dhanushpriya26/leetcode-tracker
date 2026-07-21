6a3c10a9b55ca7e0b3313c82fd6024623c163a2a
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-21 09:23:26 +0530

    Create 896-MonotonicArray

---
+++ 896-MonotonicArray/version/java/896-MonotonicArray_20260721_092326.java
@@ -0,0 +1,22 @@
+// Last updated: 7/21/2026, 9:23:26 AM
+1class Solution {
+2    public boolean isMonotonic(int[] nums) {
+3        int n = nums.length;
+4        if (n == 1)
+5             return true;
+6        boolean isInc = true;
+7        boolean isDec = true;
+8        for (int i = 1; i < n; i++) {
+9            if (!isInc && !isDec) {
+10                return false;
+11            }
+12            if (nums[i] < nums[i - 1]) {
+13                isInc = false;
+14            }
+15            if (nums[i] > nums[i - 1]) {
+16                isDec = false;
+17            }
+18        }
+19        return isInc || isDec;        
+20    }
+21}
