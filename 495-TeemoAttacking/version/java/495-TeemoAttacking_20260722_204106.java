a95202c54ddc5f1de4aa6b02994f935d0d35638d
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-07-22 20:41:06 +0530

    Create 495-TeemoAttacking

---
+++ 495-TeemoAttacking/version/java/495-TeemoAttacking_20260722_204106.java
@@ -0,0 +1,15 @@
+// Last updated: 7/22/2026, 8:41:06 PM
+1class Solution {
+2    public int findPoisonedDuration(int[] timeSeries, int duration) {
+3        int total = 0;
+4        for (int i = 0; i < timeSeries.length-1; i++) {
+5            if (timeSeries[i+1] <= timeSeries[i] + duration-1) {
+6                total += timeSeries[i+1] - timeSeries[i];
+7            } else { 
+8                total += duration;
+9            }
+10        }
+11        total += duration;
+12        return total;
+13    }
+14}
