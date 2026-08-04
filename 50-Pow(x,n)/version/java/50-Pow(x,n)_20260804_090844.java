471cd9a450e9a0a786aefc0c067a57def6603b96
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-08-04 09:08:45 +0530

    Create 50-Pow(x,n)

---
+++ 50-Pow(x,n)/version/java/50-Pow(x,n)_20260804_090844.java
@@ -0,0 +1,19 @@
+// Last updated: 8/4/2026, 9:08:44 AM
+1class Solution {
+2    public double myPow(double x, int n) {
+3        return binaryExp(x, (long) n);
+4    }
+5    private double binaryExp(double x, long n) {
+6        if (n == 0) {
+7            return 1;
+8        }  
+9        if (n < 0) {
+10            return 1.0 / binaryExp(x, -n);
+11        }
+12        if (n % 2 == 1) {
+13            return x * binaryExp(x * x, (n - 1) / 2);
+14        } else {
+15            return binaryExp(x * x, n / 2);
+16        }
+17    }
+18}
