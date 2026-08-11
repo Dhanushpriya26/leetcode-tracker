ba93e1401017f17f7b1c2e3a17add44c06f2c7fb
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-08-11 20:35:37 +0530

    Create 1837-DailyLeadsAndPartners

---
+++ 1837-DailyLeadsAndPartners/1837-DailyLeadsAndPartners.sql
@@ -0,0 +1,8 @@
+-- Last updated: 8/11/2026, 8:35:36 PM
+SELECT
+    date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) AS unique_leads,
+    COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id, make_name;
