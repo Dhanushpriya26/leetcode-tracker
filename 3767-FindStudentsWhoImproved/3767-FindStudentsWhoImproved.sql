1f1f7e4a987ea531f39b352992fce7b633475ad7
Dhanushpriya26 <24205016@drngpit.ac.in>
2026-08-11 20:33:29 +0530

    Create 3767-FindStudentsWhoImproved

---
+++ 3767-FindStudentsWhoImproved/3767-FindStudentsWhoImproved.sql
@@ -0,0 +1,13 @@
+-- Last updated: 8/11/2026, 8:33:29 PM
+# Write your MySQL query statement below
+WITH Ranked AS (
+    SELECT
+    student_id,
+    subject,
+    FIRST_VALUE(score) OVER(PARTITION BY student_id,subject ORDER BY exam_date) AS first_score,
+    FIRST_VALUE(score) OVER(PARTITION BY student_id,subject ORDER BY exam_date DESC) AS latest_score
+    FROM Scores
+)
+SELECT DISTINCT * FROM Ranked
+WHERE first_score<latest_score
+ORDER BY student_id,subject
